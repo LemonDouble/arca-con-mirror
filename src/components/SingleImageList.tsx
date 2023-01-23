f5f5670b388e620f3818ffb6a28c41b6ea34a6d8
lemondouble <lemondouble2@gmail.com>
2023-01-24 01:17:10 +0900

    feat : ZIP 다운로드 기능 추가

--- src/components/SingleImageList.tsx
+++ src/components/SingleImageList.tsx
@@ -2,7 +2,10 @@ import * as React from 'react';
 import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
 import Image from "next/image";
-import {Stack, Typography} from "@mui/material";
+import {Button, Stack, Typography} from "@mui/material";
+import JSZip from "jszip";
+import axios from "axios";
+import { saveAs } from 'file-saver';
 
 type Props ={
     title : string,
@@ -14,9 +17,32 @@ type Props ={
 
 
 export default function SingleImageList(props : Props) {
+
+    async function handleZipDownload(){
+        const zip = new JSZip()
+
+        const downloadPromise = props.imageList.map(
+            (image) => image.src
+        ).map(
+            async (src) => {
+                    const data = (await axios.get(src, {responseType: "arraybuffer"})).data
+                    zip.file(src.split("/").at(-1)!, data)
+            }
+        )
+
+        await Promise.all(downloadPromise)
+
+        const result = await zip.generateAsync({type: "blob"})
+
+        await saveAs(result, `${props.title}.zip`)
+    }
+
     return (
         <Stack spacing={1} direction="column">
-            <Typography variant="h6">{props.title}</Typography>
+            <Stack spacing={1} direction="row">
+                <Typography variant="h6">{props.title}</Typography>
+                <Button variant="contained" color="success" onClick={handleZipDownload}>ZIP 다운받기</Button>
+            </Stack>
 
             <ImageList sx={{
                 width: "80vw",
