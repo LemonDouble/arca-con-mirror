a6b1dddb94d2f803d9cddffded842d888d15f3e2
lemondouble <lemondouble2@gmail.com>
2023-01-23 19:21:48 +0900

    feat : Github Pages 배포 위해 설정 추가

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig = {
+  assetPrefix: isProd ? '/arca-con-mirror/' : '',
+
   experimental: {
     appDir: true,
+  },
+  images: {
+    unoptimized: true,
   },
 }
 
