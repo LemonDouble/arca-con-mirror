3dbcdd279af86aa7e8d2ba280c36acd9f5a4cc5a
lemondouble <lemondouble2@gmail.com>
2023-01-23 20:06:54 +0900

    fix

--- next.config.js
+++ next.config.js
@@ -1,8 +1,20 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production'
+
+// https://www.viget.com/articles/host-build-and-deploy-next-js-projects-on-github-pages/
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+let assetPrefix = ''
+let basePath = '/'
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
 
 const nextConfig = {
-  assetPrefix: isProd ? '/arca-con-mirror/' : '',
+  assetPrefix: assetPrefix,
+  basePath: basePath,
   reactStrictMode: true,
 
   images: {
