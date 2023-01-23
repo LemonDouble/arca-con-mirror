9c453355d6ddc0daea3d018750b791ec6c6acbd0
lemondouble <lemondouble2@gmail.com>
2023-01-23 16:19:40 +0900

    feat : Typescript 설정 및 크롤링 로직 추가

---
+++ src/crwaler/lib/s3.ts
@@ -0,0 +1,46 @@
+import stream from 'stream'
+import axios from "axios";
+import { S3Client } from "@aws-sdk/client-s3";
+import {Upload} from "@aws-sdk/lib-storage";
+
+export interface ImageMetaData {
+    src : string,
+    dataId : number,
+
+    extension : string,
+}
+
+export async function CopyImageToS3(arcaCondId: number, imageMetaData : ImageMetaData){
+    let contentType = 'application/octet-stream'
+    let promise = null
+
+    const uploadStream = () => {
+        const pass = new stream.PassThrough();
+        promise = new Upload({
+            client: new S3Client({region : process.env.AWS_REGION}),
+            params :{
+                Bucket : process.env.S3_BUCKET_NAME,
+                Key: `${arcaCondId}/${imageMetaData.dataId}.${imageMetaData.extension}`,
+                Body: pass,
+                ContentType: contentType,
+            }
+        })
+
+        promise.done()
+
+        return pass;
+    }
+
+    axios({
+        method: 'get',
+        url: imageMetaData.src,
+        responseType: 'stream'
+    }).then( (response) => {
+        if(response.status===200){
+            contentType = response.headers['content-type']!;
+            response.data.pipe(uploadStream());
+        }
+    });
+
+    return promise
+}
