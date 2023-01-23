1db972abd97783f627adec047baf2a03c947d6d8
lemondouble <lemondouble2@gmail.com>
2023-01-23 22:01:48 +0900

    title 추가

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,6 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
+        <title>아카콘 미러</title>
       <Head />
       <body>
         <Main />
