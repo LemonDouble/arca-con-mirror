61708969d600632f0d53a8497d33fa8367490c84
LemonDouble <31124212+LemonDouble@users.noreply.github.com>
2023-01-24 12:51:28 +0000

    Deploying to gh-pages from @ LemonDouble/arca-con-mirror@89851c5143982aae679077ae926b894201e662ec 🚀

---
+++ _next/static/_D318jsRC543f6D439kJv/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/429-a6eb43412390ca96.js","static/chunks/pages/index-5f3c13e7abedfba3.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
