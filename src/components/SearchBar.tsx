a5a37527d585723443e3a0bb385cd21e1d7faa0b
lemondouble <31124212+LemonDouble@users.noreply.github.com>
2023-12-24 03:01:36 +0900

    fix : 걍 모바일 페이지에서 320px로 고정..

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -7,7 +7,7 @@ interface Props {
 }
 export function SearchBar({value, onChange}: Props){
     return (
-        <div className="xl:w-[790px] h-[80px] bg-white-3 rounded-2xl flex flex-row space-x-4 items-center mt-5 p-2 xl:p-4 mx-2">
+        <div className="w-[320px] xl:w-[790px] h-[80px] bg-white-3 rounded-2xl flex flex-row space-x-4 items-center mt-5 p-2 xl:p-4 mx-2">
             <div className="flex-none">
                 <SearchIcon width={35} height={35}/>
             </div>
