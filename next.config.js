87133c6d6d510290e75c4b0c18dff7894d9367c5
lemondouble <31124212+LemonDouble@users.noreply.github.com>
2023-12-24 01:21:06 +0900

    feat : assets prefix 추가

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
+const isProduction = process.env.NODE_ENV === 'production'
+
+const prefix = isProduction? "https://lemondouble.github.io/arca-con-mirror/" : ""
+
 const nextConfig = {
-    output : "export"
+    output : "export",
+    images : {
+        unoptimized : true
+    },
+    assetPrefix: prefix
 }
 
 module.exports = nextConfig
