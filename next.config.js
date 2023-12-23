0c8f06b06ca675f969271dfc9723cdf90844e9b5
lemondouble <31124212+LemonDouble@users.noreply.github.com>
2023-12-24 01:04:54 +0900

    feat : tailwind 및 Nextjs 13으로 프로젝트 초기화

--- next.config.js
+++ next.config.js
@@ -1,16 +1,6 @@
-const isProduction = process.env.NODE_ENV === 'production'
-
-const prefix = isProduction? "https://lemondouble.github.io/arca-con-mirror/" : ""
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false,
-  images: {
-    unoptimized: true,
-    domains : ['dqbobx5h4f3nm.cloudfront.net']
-  },
-  assetPrefix: prefix
-
+    output : "export"
 }
 
 module.exports = nextConfig
