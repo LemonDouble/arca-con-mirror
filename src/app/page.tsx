bc74a78d1b23ed46600ad886771c6288eb904b82
LemonDouble <lemondouble2@gmail.com>
2024-11-03 14:30:01 +0900

    feat : 신규 API 이전사항 반영

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,90 +1,71 @@
 "use client"
-import {SearchBar} from "@/components/SearchBar";
-import {LemonLogo} from "@/components/icons/LemonLogo";
-import React, {useEffect, useState} from "react";
-import {useThrottle} from "@/hooks/useThrottle";
+import { SearchBar } from "@/components/SearchBar";
+import { LemonLogo } from "@/components/icons/LemonLogo";
+import React, { useEffect, useState } from "react";
+import { useThrottle } from "@/hooks/useThrottle";
 import Image from "next/image";
-import {SearchResult, StatusResult} from "@/types";
+import { SearchResult, } from "@/types";
 import Link from "next/link";
 
 export default function Home() {
     const [searchText, setSearchText] = useState("")
     const throttledInputText = useThrottle(searchText, 1000)
     const [searchResult, setSearchResult] = useState<SearchResult>({
-        hits : [], query : "", processingTimeMs : 0, limit : 0, offset : 0, estimatedTotalHits :0
+        data: [], total_page: 1, total_elements: 0
     })
-    const [totalCount, setTotalCount] = useState(0)
 
     useEffect(() => {
-        async function handleSearchTextChange(){
-            const response = await fetch("https://meili.rabbitprotocol.com/indexes/arca-con/search", {
-                method : "POST",
+        async function handleSearchTextChange() {
+            const response = await fetch("https://search.lemondouble.com/arca_con/search", {
+                method: "POST",
                 headers: {
-                    'Content-Type' : 'application/json',
-                    'Authorization' : 'Bearer 6650f03192cafd48c3cce16810b0cf588690e501eb883ac0b5ae6b8df03d68ef'
+                    'Content-Type': 'application/json',
                 },
-                body : JSON.stringify({
-                    "q": throttledInputText,
-                    "attributesToSearchOn" : ["text"],
-                    "attributesToHighlight" :["text"],
-                    "highlightPreTag": "<search-highlight>",
-                    "highlightPostTag": "</search-highlight>",
-                    "limit": 101,
-                    "offset": 0
+                body: JSON.stringify({
+                    "query": throttledInputText,
+                    "page": 1,
+                    "size": 40
                 })
             })
-            const result : SearchResult = await response.json()
+            const result: SearchResult = await response.json()
             setSearchResult(result)
         }
 
-        async function handleTotalCount(){
-            const response = await fetch("https://meili.rabbitprotocol.com/indexes/arca-con/stats", {
-                method : "GET",
-                headers: {
-                    'Content-Type' : 'application/json',
-                    'Authorization' : 'Bearer 6650f03192cafd48c3cce16810b0cf588690e501eb883ac0b5ae6b8df03d68ef'
-                }
-            })
-
-            const result : StatusResult = await response.json()
-            setTotalCount(result.numberOfDocuments)
-        }
-
         handleSearchTextChange()
-        handleTotalCount()
 
     }, [throttledInputText]);
 
-  return (
-    <main className="w-screen h-auto min-h-screen overflow-hidden">
-        <header className="w-screen h-[80px] bg-lemon-yellow px-4 flex flex-row space-x-3 items-center">
-            <LemonLogo width={32} height={32} />
-            <span className="text-2xl font-extrabold">LemonDouble</span>
-        </header>
-        <div id="container" className="h-full xl:mx-[360px] flex flex-col items-center">
-            <span className="text-3xl xl:text-6xl font-extrabold text-black-1 mt-12 text-nowrap">아카콘 미러</span>
-            <span className="text-xl xl:text-3xl font-extrabold text-black-3 mt-3 text-nowrap">그런데 이제 대사로도 검색이 되는</span>
-            <br />
-            <span className="text-base text-black-1"> 현재 검색가능한 콘 개수 : {totalCount}개</span>
-            <SearchBar
-                value={searchText}
-                onChange={(e) => setSearchText(e.target.value)}
-            />
-            <div id="stack" className="mt-2 flex flex-col space-y-2 mx-2">
-                {searchResult.hits.map(item => (
-                    <div className="xl:w-[790px] p-2 xl:p-8 flex flex-row space-x-5 bg-white-1 border-2 border-b-black-1 rounded-2xl" key={item.url}>
-                        <Image src={item.url} alt={"아카콘 이미지"} width={100} height={100} />
-                        <div className="flex flex-col grow">
-                            <span className="text-2xl font-bold text-black-1">{item.con_title}</span>
-                            <Link href={`https://arca.live/e/${item.con_number}`} target="_blank">
-                                <span className="text-lg font-normal text-black-3">{`https://arca.live/e/${item.con_number}`}</span>
-                            </Link>
-                            <span className="mt-2 text-lg font-semibold text-black-1" dangerouslySetInnerHTML={{__html : item._formatted.text}}></span>
+    return (
+        <main className="w-screen h-auto min-h-screen overflow-hidden">
+            <header className="w-screen h-[80px] bg-lemon-yellow px-4 flex flex-row space-x-3 items-center">
+                <LemonLogo width={32} height={32} />
+                <span className="text-2xl font-extrabold">LemonDouble</span>
+            </header>
+            <div id="container" className="h-full xl:mx-[360px] flex flex-col items-center">
+                <span className="text-3xl xl:text-6xl font-extrabold text-black-1 mt-12 text-nowrap">아카콘 미러</span>
+                <span className="text-xl xl:text-3xl font-extrabold text-black-3 mt-3 text-nowrap">그런데 이제 대사로도 검색이 되는</span>
+                <br />
+                {searchResult.total_elements != 0 && <span className="text-base text-black-1"> 총 검색된 콘 개수 : {searchResult.total_elements}개</span>}
+                <SearchBar
+                    value={searchText}
+                    onChange={(e) => setSearchText(e.target.value)}
+                />
+                <div id="stack" className="mt-2 flex flex-col space-y-2 mx-2">
+                    {searchResult.data.map(item => (
+                        <div className="xl:w-[790px] p-2 xl:p-8 flex flex-row space-x-5 bg-white-1 border-2 border-b-black-1 rounded-2xl" key={item.url}>
+                            <Image src={item.url} alt={"아카콘 이미지"} width={100} height={100} />
+                            <div className="flex flex-col grow">
+                                <span className="text-2xl font-bold text-black-1">{item.con_title}</span>
+                                <Link href={`https://arca.live/e/${item.con_number}`} target="_blank">
+                                    <span className="text-lg font-normal text-black-3">{`https://arca.live/e/${item.con_number}`}</span>
+                                </Link>
+                                <span className="mt-2 text-lg font-semibold text-black-1" dangerouslySetInnerHTML={{ __html: item.text }}></span>
+                            </div>
                         </div>
-                    </div>
-                ))}
+                    ))}
+
+                </div>
             </div>
-        </div>
-    </main>
-  )
+        </main>
+    )
 }
--- src/types.ts
+++ src/types.ts
@@ -1,42 +1,12 @@
 export interface SearchResult {
-    hits: Hit[]
-    query: string
-    processingTimeMs: number
-    limit: number
-    offset: number
-    estimatedTotalHits: number
+    total_page: number
+    total_elements: number
+    data : ArcaCon[]
 }
 
-export interface Hit {
-    uid: string
-    con_number: number
+export interface ArcaCon{
+    con_number : number
     con_title: string
-    url: string
-    text: string
-    confidence: number
-    _formatted: Formatted
-}
-
-export interface Formatted {
-    uid: string
-    con_number: string
-    con_title: string
-    url: string
-    text: string
-    confidence: string
-}
-
-export interface StatusResult {
-    numberOfDocuments: number
-    isIndexing: boolean
-    fieldDistribution: FieldDistribution
-}
-
-export interface FieldDistribution {
-    con_number: number
-    con_title: number
-    confidence: number
-    text: number
-    uid: number
-    url: number
+    text : string
+    url : string
 }
